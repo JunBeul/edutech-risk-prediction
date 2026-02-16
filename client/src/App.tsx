63e7848128e7587d7ec686f9c17eb09b6f1bc6ac
JunBeul <yhs06280@naver.com>
2026-02-16 16:09:12 +0900

    feat: sass(scss) support, .env.example, and some refactoring

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 ﻿import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
 
 function App() {
   const [count, setCount] = useState<number>(0)
